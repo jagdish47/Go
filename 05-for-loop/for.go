c514871a12d0095a9a7044cb9855e04c0d0cce99
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2024-07-15 08:36:14 +0530

    for loop, break and continue, range

---
+++ 05-for-loop/for.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+//there is no while keyword in go.
+//for looping only for we use, using for we create while loop as well
+func main(){
+
+
+
+	// 1
+	//while loop(using for loop)
+	// i:=1
+	// for i <=5 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// 2
+	// infinite loop
+	// for{}
+
+	// 3
+	// for loop
+	// for i:=1; i<=5; i++{
+	// 	if i == 3{
+	// 		break
+	// 	}
+	// 	if i == 1{
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// 4
+	// Range 
+	// for i:= range 10 {
+	// 	fmt.Println(i);
+	// }
+
+}
+
+
+
